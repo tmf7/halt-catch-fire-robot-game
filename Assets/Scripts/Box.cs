de0c8889b1799b859fd1824c48b93811d9dd73d8
Tom Freehill <tmf7@njit.edu>
2017-03-30 22:52:45 -0400

    finalizing box thrower prefab and box script

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+	public GameObject 		dropShadowPrefab;
+	
+	[HideInInspector]
+	public float 			airTime;
+
+	private GameObject		dropShadow;
+	private Vector3 		shadowVelocity;
+	private BoxCollider2D 	boxCollider;
+	private Rigidbody2D 	boxRB;
+	private float			strikeTime;
+	private bool 			grounded;
+
+	void Awake() {
+		// FIXME: non-zero parent is screwing with the calculations (do not parent it to the box, it amplifies the velocity/movement)
+		dropShadow = Instantiate<GameObject> (dropShadowPrefab, transform.position, Quaternion.identity);
+	}
+
+	void Start () {
+		boxCollider = GetComponent<BoxCollider2D> ();
+		boxCollider.enabled = false;
+		boxRB = GetComponent<Rigidbody2D> ();
+
+		// predicted landing point
+		float xFinal = transform.position.x + (boxRB.velocity.x * airTime);
+		float yFinal = transform.position.y + (boxRB.velocity.y * airTime) + (0.5f * Physics2D.gravity.y * airTime * airTime);
+
+		// ground movement of the drop shadow
+		Vector3 shadowDir = new Vector3 (xFinal, yFinal, 0.0f) - transform.position;
+		float distance = shadowDir.magnitude;
+		shadowDir.Normalize ();
+		float shadowSpeed = distance / airTime;
+		shadowVelocity = new Vector3 (shadowSpeed * shadowDir.x, shadowSpeed * shadowDir.y, 0.0f);
+
+		strikeTime = Time.time + airTime;
+		grounded = false; 
+	}
+
+	void Update() {
+		if (grounded)
+			return;
+
+		dropShadow.transform.position += shadowVelocity * Time.deltaTime;
+
+		if (Time.time > strikeTime) {
+			grounded = true;
+			boxRB.gravityScale = 0;
+			boxRB.velocity = Vector2.zero;
+			boxRB.isKinematic = true;
+		}
+	}
+
+	// FIXME: they start off touching, only enable on the return trip
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.tag == "Shadow")
+			dropShadow.SetActive(false);//boxCollider.enabled = true;
+	}
+}
--- Assets/Scripts/BoxThrower.cs
+++ Assets/Scripts/BoxThrower.cs
@@ -1,29 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BoxThrower : MonoBehaviour {
 
-	public GameObject boxSprite;	// the prefab to throw
+	public Box 			boxPrefab;
+	public float 		throwDelay;
 
-	private Rigidbody2D boxRB2D;
+	private List<Box> 	allBoxes;		// TODO: move this to a singleton GameManager
+	private float 		nextThrowTime;
 
-	void Awake() {
-		boxRB2D = boxSprite.GetComponent<Rigidbody2D> ();
-
+	void Start() {
+		nextThrowTime = Time.time + throwDelay;
+		allBoxes = new List<Box>();
 	}
 
-	void Start () {
-		// 1) the parent transform of the box starts at ground (affects both 
-		// 2) the 3d rb is given a purely z-velocity
-		// 3) the 2d rb is given an equivalent muzzle velocity at an angle (such that it will hit a specific x,y position on the map)
-		// 4) 2d collision is disabled until the 3d rb reaches a set height (then the 2d collision box can hit robots and walls, etc)
-		// 5) 2d rb gravity.scale is zeroed as soon as the 3d rb makes contact (so the box doesn't slide to the bottom of the screen)
-	}
-	
-	// Update is called once per frame
+	// 1) all transforms start equivalent to this boxThrower
+	// 2) the 3d rb is given a purely z-velocity
+	// 3) the 2d rb is given an equivalent muzzle velocity at an angle (such that it will hit a specific x,y position on the map)
+	// 4) 2d collision is disabled until the 3d rb reaches a set height (then the 2d collision box can hit robots and walls, etc) [in Box.cs]
+	// 5) 2d rb gravity.scale is zeroed as soon as the 3d rb makes contact (so the box doesn't slide to the bottom of the screen) [in Box.cs]
+
 	void Update () {
-		//		boxRB2D.gravityScale = 0;  set this once the 3d rb reaches a certain height
-		
+		if (Time.time > nextThrowTime) {
+			nextThrowTime = Time.time + throwDelay;
+
+			float throwSpeed = Random.Range (8.0f, 12.0f);
+			float throwAngle = Random.Range (30.0f * Mathf.Deg2Rad, 150.0f * Mathf.Deg2Rad);
+			float airTime = Random.Range (0.5f, 2.0f);
+
+			// FIXME: non-zero parent is screwing with the calculations
+			Box thrownBox = Instantiate<Box>(boxPrefab, transform.position, Quaternion.identity);
+			allBoxes.Add (thrownBox);
+			thrownBox.airTime = airTime;
+
+			Rigidbody2D boxRB;
+			boxRB = thrownBox.GetComponent<Rigidbody2D> ();
+			boxRB.velocity = new Vector2(throwSpeed * Mathf.Cos(throwAngle), throwSpeed * Mathf.Sin(throwAngle));
+		}
 	}
 }
